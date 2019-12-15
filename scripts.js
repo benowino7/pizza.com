428c2323786a82268b131d7e6e64ae8aeaabf435
benson7-coder <bensonowino7@gmail.com>
2019-12-15 20:53:20 +0300

    added  a function that displays the contact section

--- scripts.js
+++ scripts.js
@@ -99,3 +99,26 @@ function display()
         default: alert(_f('dtls')[4].value + 'invalid Selection display'); break;
     }
 }
+
+function checkEmpty() {
+    var name = document.forms['forms'].elements[0].value;
+    var email = document.forms['forms'].elements[1].value;
+    var message = document.forms['forms'].elements[2].value;
+
+    if (name == "" && email == "") {
+        alert('please enter your details');
+        return 0;
+    }
+    else {
+        if (name == "") {
+            alert("please enter your name!");
+        }
+        else if (email == "") {
+            alert("please enter your email!");
+        }
+    }
+    if ((name !== "" && email != "" && text == "") || (name != "" && email != "" && text != "")) {
+        alert("Dear " + name + " your message was received successfully.");
+        document.getElementById('results').innerHTML = "Dear " + name + " your message was received successfully you will be contacted soon. Thank you.";
+    }
+}
